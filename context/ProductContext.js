aeea81a64c4fb03e66f12e36613d9eb17f16b69b
SlavKoVrn <slavko.chita@gmail.com>
2025-03-20 18:13:52 +0300

    kadastrcard rest api

--- context/ProductContext.js
+++ context/ProductContext.js
@@ -14,7 +14,7 @@ export const ProductProvider = ({ children }) => {
   // Fetch products from the API
   const fetchProducts = async () => {
     try {
-      const response = await fetch("https://fakestoreapi.com/products");
+      const response = await fetch("http://restapi.kadastrcard.ru/api/products");
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
